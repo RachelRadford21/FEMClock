672c9e364a25732f0cd3e2c311d8d61fa46ed74a
Rachel Radford <rachelradford21@gmail.com>
2022-07-08 17:37:18 -0400

    Initial Commit

---
+++ FEMClock/FEMClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FEMClockApp.swift
+//  FEMClock
+//
+//  Created by Rachel Radford on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FEMClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
